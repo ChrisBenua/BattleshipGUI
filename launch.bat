6cad264c090266380d9a1550632355fa2baead1e
ChrisBenua <kpbenua@edu.hse.ru>
2020-03-30 20:20:03 +0300

    launch files and readme

---
+++ launch.bat
@@ -0,0 +1,2 @@
+SET PATH_TO_FX="C:\Program Files\Java\Distribs\javafx-sdk-11.0.2"
+java --module-path %PATH_TO_FX%/lib --add-modules javafx.graphics,javafx.controls -jar BattleshipGUI.jar
---
+++ launch.sh
@@ -0,0 +1,2 @@
+export PATH_TO_FX="/Users/Christian/Documents/JavaHSE/distribs/javafx-sdk-11.0.2"
+java --module-path $PATH_TO_FX/lib --add-modules javafx.graphics,javafx.controls -jar BattleshipGUI.jar
